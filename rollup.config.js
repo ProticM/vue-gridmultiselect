1cf0aed74dad1ad9e628811d5480ac3b114576e7
Milos <protic023@gmail.com>
2019-11-25 10:47:53 +0100

    updated rollup to produce esm bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,28 @@
-  
+
 import babel from 'rollup-plugin-babel';
 import vue from 'rollup-plugin-vue';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    input: 'src/index.js',
-    output: {
-        name: 'vuegridmultiselect',
-        file: 'dist/vue-multiselect.js',
-        format: 'umd'
-    },
-    plugins: [
-        commonjs(),
-        resolve(),
-        vue({ css: true }),
-        babel({
-            extensions: [ '.js', '.vue' ],
-            exclude: ['node_modules/**'],
-            presets: ['@babel/preset-env']
-        })
-    ]
+	input: 'src/index.js',
+	output: [{
+		name: 'vuegridmultiselect',
+		file: 'dist/vue-multiselect.js',
+		format: 'umd'
+	},
+	{
+		file: 'dist/vue-multiselect-esm.js',
+		format: 'esm'
+	}],
+	plugins: [
+		commonjs(),
+		resolve(),
+		vue({ css: true }),
+		babel({
+			extensions: ['.js', '.vue'],
+			exclude: ['node_modules/**'],
+			presets: ['@babel/preset-env']
+		})
+	]
 }
