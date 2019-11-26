4ce5498495d4e6d6f9000cf06b6a5e8c61ea0eef
Milos <protic023@gmail.com>
2019-11-26 11:58:02 +0100

    updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,33 @@ import babel from 'rollup-plugin-babel';
 import vue from 'rollup-plugin-vue';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
+export default [{
 	input: 'src/index.js',
 	output: [{
 		name: 'VueGridMultiselect',
 		file: 'dist/vue-multiselect.js',
 		format: 'umd'
-	},
-	{
-		file: 'dist/vue-multiselect-esm.js',
-		format: 'esm'
 	}],
+	plugins: [
+		commonjs(),
+		resolve(),
+		vue({ css: true }),
+		babel({
+			extensions: ['.js', '.vue'],
+			exclude: ['node_modules/**'],
+			presets: ['@babel/preset-env']
+		}),
+		uglify()
+	]
+}, {
+	input: 'src/index.js',
+	output: [
+		{
+			file: 'dist/vue-multiselect-esm.js',
+			format: 'esm'
+		}],
 	plugins: [
 		commonjs(),
 		resolve(),
@@ -25,4 +40,4 @@ export default {
 			presets: ['@babel/preset-env']
 		})
 	]
-}
+}];
