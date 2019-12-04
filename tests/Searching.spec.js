cf99fc9ae184e5631c9611db4b49294e7fadbc2b
Milos <protic023@gmail.com>
2019-12-04 14:13:55 +0100

    updated tests

--- tests/Searching.spec.js
+++ tests/Searching.spec.js
@@ -23,5 +23,18 @@ describe('searching', () => {
 		expect(
 			wrapper.find('.gridmultiselect__searchfield-wrap').exists()
 		).toEqual(false);
+	});
+
+	it('should find text item 1', () => {
+		const wrapper = wrapShallow({
+			value: null,
+			items: [{ id: 1, text: 'Item 1' }, { id: 2, text: 'Item 2' }]
+		}), input = wrapper.find('.gridmultiselect__searchfield');
+
+		input.element.value = 1;
+		input.trigger('input');
+
+		expect(wrapper.vm.searchTerm).toEqual('1');
+		expect(wrapper.vm.internalItems).toEqual([{ id: 1, text: 'Item 1' }]);
 	})
 })
