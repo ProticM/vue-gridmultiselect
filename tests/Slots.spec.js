60028d0b3f8366fd04cbcdc41b749b10f33ca6a6
Milos <protic023@gmail.com>
2019-12-16 14:09:23 +0100

    updated scoped slot tests

--- tests/Slots.spec.js
+++ tests/Slots.spec.js
@@ -16,6 +16,24 @@ describe('slots', () => {
 		expect(slot.exists()).toBe(true);
 	});
 
+	it('should have items footer slot', () => {
+
+		const wrapper = wrapShallowWithOptions({
+			value: null,
+			items: [{ id: 1, text: 'Item 1' }, { id: 2, text: 'Item 2' }]
+		}, {
+			slots: {
+				'items-footer': "<div class=\"items-footer-slot\">Footer slot</div>"
+			}
+		});
+
+		const slotWrapper = wrapper.find("li.gridmultiselect__itemsfooter");
+		expect(slotWrapper.exists()).toBe(true);
+		const slot = slotWrapper.find("div.items-footer-slot");
+		expect(slot.exists()).toBe(true);
+		expect(slot.text()).toBe('Footer slot');
+	});
+
 	it('should have selected item scoped slot', () => {
 
 		const wrapper = wrapShallowWithOptions({
@@ -54,5 +72,28 @@ describe('slots', () => {
 		slot = items.at(1).find('span');
 		expect(slot.exists()).toBe(true);
 		expect(slot.text()).toBe('Item 2');
+	});
+
+	it('should have row details slot', () => {
+		const wrapper = wrapShallowWithOptions({
+			value: [{ id: 1, text: 'Item 1' }],
+			items: [{ id: 1, text: 'Item 1' }, { id: 2, text: 'Item 2' }],
+			itemDetails: 'text'
+		}, {
+			scopedSlots: {
+				'selected-item-details': `<template slot-scope="{selectedItem}"><span>{{selectedItem.text}}</span></template>`
+			}
+		});
+
+		expect(wrapper.vm.isRowDetailEnabled).toEqual(true);
+
+		wrapper.vm.toggleDetails(wrapper.vm.selectedItems[0]);
+		const details = wrapper.findAll('.gridmultiselect__selecteditemdetails');
+
+		expect(details.length).toEqual(1);
+		const detailsSlot = details.at(0).find('span');
+
+		expect(detailsSlot.exists()).toBe(true);
+		expect(detailsSlot.text()).toBe('Item 1');
 	});
 })
