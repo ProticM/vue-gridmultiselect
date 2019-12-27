b018dc0105bc6d68f5fa7f4fd0165a03d31824e3
Milos <protic023@gmail.com>
2019-12-27 08:44:43 +0100

    added banner to build files (js)

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 import css from 'rollup-plugin-css-only'
+import pck from './package.json';
 
 const extensions = [
 	'.js', '.vue'
@@ -21,20 +22,31 @@ const plugins = [
 	})
 ];
 
+const buildBanner = () => {
+	return [
+		'/*!',
+		'* Vue GridMultiselect v' + pck.version,
+		'* Copyright ' + pck.author + ' and other contributors',
+		'* Licensed under ' + pck.license + ' (https://github.com/ProticM/plazar-js/blob/master/LICENSE)',
+		'*/'
+	].join('\n');
+};
+
 const input = 'src/index.js';
 const getOutput = (format, isMin) => {
 	const isUMD = format === 'umd';
 	return {
 		file: `dist/vue-gridmultiselect${isMin ? '.min' : ''}${isUMD ? '' : '.esm'}.js`,
 		format: format,
-		...isUMD ? { name: 'VueGridMultiselect' } : null
+		...isUMD ? { name: 'VueGridMultiselect' } : null,
+		banner: buildBanner()
 	};
 };
 
 export default [{
 	input: input,
 	output: [getOutput('umd', true)],
-	plugins: plugins.concat(uglify())
+	plugins: plugins.concat(uglify({ output: { comments: "/^!/" } }))
 }, {
 	input: input,
 	output: [getOutput('umd', false)],
