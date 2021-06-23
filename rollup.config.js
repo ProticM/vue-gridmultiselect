ccb2f542f79cf688fcb6b85f6849fdce4cbde5a5
ProticM <protic023@gmail.com>
2021-06-23 15:31:55 +0200

    updated packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
-
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import vue from 'rollup-plugin-vue';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
-import css from 'rollup-plugin-css-only'
+import css from 'rollup-plugin-css-only';
 import pck from './package.json';
 
 const extensions = [
@@ -25,7 +24,8 @@ const plugins = [
 	}),
 	babel({
 		extensions: extensions,
-		exclude: ['node_modules/**']
+		exclude: ['node_modules/**'],
+		babelHelpers: 'bundled'
 	})
 ];
 
